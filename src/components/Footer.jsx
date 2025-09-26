74d15e08e05f5e514fca4fea5e27ca9a31d852de
leeosilvp <leo965679@gmail.com>
2025-09-26 18:00:06 -0300

    46 u- atualizando prioridade do botão

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../css/footer.css'
 function Footer() {
     return (
         <footer>
-            <h1>Graham | V.0.4.5</h1>
+            <h1>Graham | V.0.4.6</h1>
         </footer>
     )
 }
