ded55581db29f7e03e70274e6cb27a32a58a3f48
leeosilvp <leo965679@gmail.com>
2025-09-27 01:32:06 -0300

    62 r- removendo Highlight GitHub

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,7 +4,6 @@ import { sendMessageToAI } from "../services/sendMessage.js";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
-import 'highlight.js/styles/github-dark.css';
 import '../css/markdown.css'
 
 function Chat() {
