a3934a74a3eec7a7b4d9d1f160a13276fcf7b2dc
leeosilvp <leo965679@gmail.com>
2025-09-27 01:58:14 -0300

    66 m- modificando estilização da resposta da IA

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../css/footer.css'
 function Footer() {
     return (
         <footer>
-            <h1>Graham | V.0.6.5</h1>
+            <h1>Graham | V.0.6.6</h1>
         </footer>
     )
 }
