5f118cb1f948cc25af93b36d4449563aff246315
leeosilvp <leo965679@gmail.com>
2025-09-28 16:08:24 -0300

    80 m- alterando toda estrutura de renderização LATEX para melhor controle

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../css/footer.css'
 function Footer() {
     return (
         <footer>
-            <h1>Graham | V.0.7.9</h1>
+            <h1>Graham | V.0.8.0</h1>
         </footer>
     )
 }
