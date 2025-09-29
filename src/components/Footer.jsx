9f1c9df114ad7a226aacfaccfbf3e460854fb984
leeosilvp <leo965679@gmail.com>
2025-09-28 23:29:26 -0300

    94 a- adicionando scroolbar no chat

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../css/footer.css'
 function Footer() {
     return (
         <footer>
-            <h1>Graham | V.0.9.3</h1>
+            <h1>Graham | V.0.9.4</h1>
         </footer>
     )
 }
