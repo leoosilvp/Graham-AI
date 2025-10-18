3f5f977a046ee626df2bcc9de57384ea87470a03
leeosilvp <leo965679@gmail.com>
2025-10-18 15:32:22 -0300

    126 u- controle de versão

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../css/footer.css'
 function Footer() {
     return (
         <footer>
-            <h1>Graham | V.1.2.5</h1>
+            <h1>Graham | V.1.2.6</h1>
         </footer>
     )
 }
