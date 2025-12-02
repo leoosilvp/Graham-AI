4347c12f9d94f66f852b17ef5e5a031de9fce25f
leeosilvp <leo965679@gmail.com>
2025-12-02 20:34:28 -0300

    a- criando conteudo da area Geral de settings

--- src/components/ui/General.jsx
+++ src/components/ui/General.jsx
@@ -4,6 +4,25 @@ const General = () => {
             <section className="settings-area-header">
                 <h1>Geral</h1>
             </section>
+            <section className="settings-change">
+                <h2>Tema</h2>
+                <button>Sistema <i className="fa-solid fa-chevron-down"></i></button>
+            </section>
+
+            <hr />
+
+            <section className="settings-change">
+                <h2>Cor favorita</h2>
+                <button><div></div> Padrão <i className="fa-solid fa-chevron-down"></i></button>
+            </section>
+
+            <hr />
+
+            <section className="settings-change">
+                <h2>Idioma</h2>
+                <button>Detectar automaticamente <i className="fa-solid fa-chevron-down"></i></button>
+            </section>
+
         </section>
     )
 }
