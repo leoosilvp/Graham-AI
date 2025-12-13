fc1db2a153d53d8765f76e6f4fc2f4cd08f42288
leeosilvp <leo965679@gmail.com>
2025-12-13 00:52:30 -0300

    a- criando conteudo do feed do Welcome de Introduction

--- src/components/ui/Introduction.jsx
+++ src/components/ui/Introduction.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom"
+import video from '../../assets/video/upd.mp4'
 
 const Introduction = () => {
     return (
@@ -12,11 +13,27 @@ const Introduction = () => {
             </div>
 
             <section className='docs-feed'>
-                <h1>oi</h1>
+                <h1>Seja Bem-vindo/a</h1>
+                <h2>Saiba mais sobre Graham AI e como começar.</h2>
+
+                <video src={video} controls />
+
+                <p>O Graham AI é um assistente de IA avançado, projetado para ser claro, preciso e empático em todas as interações. Seu objetivo é não apenas responder perguntas, mas facilitar o aprendizado, resolver problemas complexos e entregar resultados confiáveis de maneira didática e acessível.</p>
+                
+                <h1>O que torna Graham AI diferente?</h1>
+                <p>Enquanto assistentes comuns se limitam a responder perguntas, o <span>Graham AI executa</span>, resolve e entrega resultados completos. Ele opera de forma autônoma em tarefas complexas, sem exigir microgerenciamento, mantendo contexto persistente em projetos longos e entregando soluções prontas para uso - desde análises detalhadas e cálculos precisos como: <span>E = mc²</span> até documentos formatados profissionalmente. Não apenas informa: resolve problemas de forma independente. Defina a tarefa e o Graham AI cuida de todo o processo, liberando você para focar no que realmente importa. Pronto para transformar sua produtividade?</p>
+                
+                <hr />
+
+                <div className="social-links">
+                    <a href=""><i className="fa-brands fa-instagram" /></a>
+                    <a href=""><i className="fa-brands fa-github" /></a>
+                    <a href=""><i className="fa-brands fa-linkedin" /></a>
+                    <a href=""><i className="fa-brands fa-youtube" /></a>
+                </div>
             </section>
 
             <div className='docs-aside-right'>
-                <h1>aki</h1>
             </div>
         </section>
     )
--- src/routes/Docs.jsx
+++ src/routes/Docs.jsx
@@ -4,36 +4,38 @@ import logo from '../assets/img/logo.svg'
 
 const Docs = () => {
     return (
-        <main className='docs-page'>
-            <header>
-                <section className="docs-header-content">
-                    <Link to='/chat'>
-                        <img src={logo} alt='logo Graham AI' />
-                    </Link>
+        <main className='ctn-docs-page'>
+            <section className='docs-page'>
+                <header>
+                    <section className="docs-header-content">
+                        <Link to='/chat'>
+                            <img src={logo} alt='logo Graham AI' />
+                        </Link>
 
-                    <section className="docs-search">
-                        <div className="sec-left-search">
-                            <i className='fa-solid fa-search' />
-                            <p>Search...</p>
-                        </div>
-                        <p>Ctrl + p</p>
+                        <section className="docs-search">
+                            <div className="sec-left-search">
+                                <i className='fa-solid fa-search' />
+                                <p>Search...</p>
+                            </div>
+                            <p>Ctrl + p</p>
+                        </section>
+
+                        <Link to='/chat/settings/account'><i className='fa-regular fa-circle-user'></i></Link>
                     </section>
 
-                    <Link to='/chat/settings/account'><i className='fa-regular fa-circle-user'></i></Link>
-                </section>
+                    <nav>
+                        <ul>
+                            <li><NavLink to='introduction' className={({ isActive }) => isActive ? 'active' : ''}>Introdução</NavLink></li>
+                            <li><NavLink to='features' className={({ isActive }) => isActive ? 'active' : ''}>Aparência</NavLink></li>
+                            <li><NavLink to='plans and services' className={({ isActive }) => isActive ? 'active' : ''}>Planos e Preços</NavLink></li>
+                            <li><NavLink to='api' className={({ isActive }) => isActive ? 'active' : ''}>APIs</NavLink></li>
+                            <li><NavLink to='enterprise' className={({ isActive }) => isActive ? 'active' : ''}>Para empresas</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
 
-                <nav>
-                    <ul>
-                        <li><NavLink to='introduction' className={({ isActive }) => isActive ? 'active' : ''}>Introdução</NavLink></li>
-                        <li><NavLink to='features' className={({ isActive }) => isActive ? 'active' : ''}>Aparência</NavLink></li>
-                        <li><NavLink to='plans and services' className={({ isActive }) => isActive ? 'active' : ''}>Planos e Preços</NavLink></li>
-                        <li><NavLink to='api' className={({ isActive }) => isActive ? 'active' : ''}>APIs</NavLink></li>
-                        <li><NavLink to='enterprise' className={({ isActive }) => isActive ? 'active' : ''}>Para empresas</NavLink></li>
-                    </ul>
-                </nav>
-            </header>
-
-            <Outlet />
+                <Outlet />
+            </section>
         </main>
     )
 }
