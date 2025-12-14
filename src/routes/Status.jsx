d76bae8c4a583f05c679f8604f85e4ccaa850ea0
leeosilvp <leo965679@gmail.com>
2025-12-14 01:27:24 -0300

    a- criando Header da page Status

--- src/routes/Status.jsx
+++ src/routes/Status.jsx
@@ -1,11 +1,16 @@
 import '../css/status.css'
+import icon from '../assets/img/icon-light.svg'
+import { NavLink } from 'react-router-dom'
 
 const Status = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <main className='status-page'>
+        <header className='status-page-header'>
+            <img src={icon} alt="Graham AI icon" />
+            <NavLink to='/docs'>Ir para Docs</NavLink>
+        </header>
+    </main>
+  ) 
 }
 
 export default Status
