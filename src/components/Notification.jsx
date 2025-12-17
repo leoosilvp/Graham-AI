d3208a4f581a93f382b86672ab31ab88f667f5a1
leeosilvp <leo965679@gmail.com>
2025-12-17 03:34:19 -0300

    a- criando scrool na pagina Notification e colocando btn para fechar

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,10 +1,19 @@
+import { Link } from 'react-router-dom'
 import '../css/notification.css'
 
 const Notification = () => {
   return (
-    <div>
-      <h1>Notificações</h1>
-    </div>
+    <main className='ctn-notification-page'>
+      <Link to='/chat' className='close-notification-btn' title='Fechar'><i className='fa-solid fa-xmark' /></Link>
+
+      <section className='notification-page'>
+        <section className='notification-header'>
+          <h1>Notificações</h1>
+
+        </section>
+      </section>
+
+    </main>
   )
 }
 
