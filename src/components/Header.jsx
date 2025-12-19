154d7ce459d3f1e8a01e2ed6e3b8d5ed40b4eb56
leeosilvp <leo965679@gmail.com>
2025-12-18 22:36:37 -0300

    a- criando baixa responsividade da page Notification

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
-import '../css/header.css'
-import logo from '../assets/img/icon-light.svg'
-import { Link } from 'react-router-dom'
+import '../css/header.css';
+import logo from '../assets/img/icon-light.svg';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
+    const location = useLocation();
+    const isNotification = location.pathname === '/notification';
+
     return (
-        <header>
+        <header className={`${isNotification ? 'header-notification' : ''}`}>
             <h1>Graham AI</h1>
-            <img src={logo} alt="logo" />
-            <Link to='https://graham-ai-page.vercel.app/'><i className="fa-solid fa-book-open"></i></Link>
+            <img onClick={() => window.location.href = '/chat'} src={logo} alt="logo" />
+            <Link to='https://graham-ai-page.vercel.app/'>
+                <i className="fa-solid fa-book-open"></i>
+            </Link>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
