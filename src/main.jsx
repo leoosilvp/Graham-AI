b6f2355392ce7da6e21511ce31667a422f7475cb
leeosilvp <leo965679@gmail.com>
2026-01-01 19:47:44 -0300

    a- ativando Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import { injectSpeedInsights } from "@vercel/speed-insights";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import App from './App.jsx'
 
-//  injectSpeedInsights()
+injectSpeedInsights()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
