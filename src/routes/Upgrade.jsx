55f617bc032cde495d689759471e5d9c578567f9
leeosilvp <leo965679@gmail.com>
2026-06-23 01:47:15 -0300

    feat: criando estrutura da seção de apresentação de Upgrade

--- src/routes/Upgrade.jsx
+++ src/routes/Upgrade.jsx
@@ -1,9 +1,21 @@
 import '../css/upgrade.css'
+import { Link } from 'react-router-dom'
+import logo from '../assets/svg/logo.svg'
+import logoPleromaGraham from '../assets/svg/logo-pleroma-graham.svg'
 
 const Upgrade = () => {
     return (
         <main className="upgrade-main">
-
+            <header className='upgrade-header'>
+                <Link to='/new'>
+                    <img src={logo} alt="" />
+                </Link>
+                <Link to='/new'>Experimentar o Graham</Link>
+            </header>
+            <section className='upgrade-presentation'>
+                <h1>Planos Individuais</h1>
+                <img src={logoPleromaGraham} />
+            </section>
         </main>
     )
 }
