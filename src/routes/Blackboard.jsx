01ba376593b5d866d1569f2bd5cdf0105d7e7f43
leeosilvp <leo965679@gmail.com>
2026-07-04 01:07:34 -0300

    fix: adicionando licensa temporaria de TlDraw

--- src/routes/Blackboard.jsx
+++ src/routes/Blackboard.jsx
@@ -15,6 +15,7 @@ const Blackboard = () => {
                 persistenceKey="example"
                 forceDarkMode={true}
                 onMount={handleMount}
+                licenseKey='tldraw-2026-07-18/ WyJsZ3hRdmpGTCIsWyIqIl0sMTYsIj IwMjYtMDctMTgiXQ. bs1Gcz9s7gskAP4XLUJhVRqA0JZXix KzE2awC01v+qwsnz8D+ RBjSQyKpJEJyCtq+ 0WhmIlIVgXNeBaARBt3Nw'
             />
         </main>
     )
